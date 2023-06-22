6ddd432a8a730864dd179e495e402abad25104c7
Stuart Smith <98703910+stusmi@users.noreply.github.com>
2023-06-22 02:09:14 -0400

    Update to Fedora 38 base and reworked build

--- Containerfile
+++ Containerfile
@@ -1,32 +1,21 @@
-ARG FEDORA_MAJOR_VERSION=37
+ARG IMAGE_NAME="${IMAGE_NAME:-silverblue}"
+ARG SOURCE_IMAGE="${SOURCE_IMAGE:-silverblue}"
+ARG BASE_IMAGE="quay.io/fedora-ostree-desktops/${SOURCE_IMAGE}"
+ARG FEDORA_MAJOR_VERSION="${FEDORA_MAJOR_VERSION:-38}"
 
-FROM ghcr.io/ublue-os/silverblue-main:${FEDORA_MAJOR_VERSION}
+FROM ${BASE_IMAGE}:${FEDORA_MAJOR_VERSION} AS builder
+
+ARG IMAGE_NAME="${IMAGE_NAME}"
+ARG FEDORA_MAJOR_VERSION="${FEDORA_MAJOR_VERSION}"
+
+ADD build.sh /tmp/build.sh
+ADD packages.json /tmp/packages.json
 
 COPY etc /etc
-# COPY usr /usr
+COPY --from=ghcr.io/ublue-os/config:latest /rpms /tmp/rpms
+COPY --from=ghcr.io/ublue-os/akmods:${FEDORA_MAJOR_VERSION} /rpms /tmp/akmods-rpms
 
-COPY silverybluish-firstboot /usr/bin
-COPY recipe.yml /etc/silverybluish-recipe.yml
-
-COPY --from=docker.io/mikefarah/yq /usr/bin/yq /usr/bin/yq
-
-RUN rpm-ostree override remove firefox firefox-langpacks
-
-RUN echo "-- Installing RPMs defined in recipe.yml --" && \
-    rpm_packages=$(yq '.rpms[]' < /etc/silverybluish-recipe.yml) && \
-    for pkg in $rpm_packages; do \
-        echo "Installing: ${pkg}" && \
-        rpm-ostree install $pkg; \
-    done && \ 
-    systemctl unmask dconf-update.service && \
-    systemctl enable dconf-update.service && \
-    systemctl enable tailscaled.service && \
-    sed -i 's/#DefaultTimeoutStopSec.*/DefaultTimeoutStopSec=15s/' /etc/systemd/user.conf && \
-    sed -i 's/#DefaultTimeoutStopSec.*/DefaultTimeoutStopSec=15s/' /etc/systemd/system.conf && \
-    echo "---"
-
-RUN rm -rf \
-        /tmp/* \
-        /var/* && \
-    rm -f /etc/yum.repos.d/tailscale.repo && \
-    ostree container commit
+RUN /tmp/build.sh
+RUN rm -rf /tmp/* /var/*
+RUN ostree container commit
+RUN mkdir -p /var/tmp && chmod -R 1777 /var/tmp
---
+++ build.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+set -ouex pipefail
+
+RELEASE="$(rpm -E %fedora)"
+
+INCLUDED_PACKAGES=($(jq -r "[(.all.include | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[]), \
+                             (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".include | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[])] \
+                             | sort | unique[]" /tmp/packages.json))
+EXCLUDED_PACKAGES=($(jq -r "[(.all.exclude | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[]), \
+                             (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".exclude | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[])] \
+                             | sort | unique[]" /tmp/packages.json))
+
+
+if [[ "${#EXCLUDED_PACKAGES[@]}" -gt 0 ]]; then
+    EXCLUDED_PACKAGES=($(rpm -qa --queryformat='%{NAME} ' ${EXCLUDED_PACKAGES[@]}))
+fi
+
+wget -P /tmp/rpms \
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${RELEASE}.noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${RELEASE}.noarch.rpm
+
+rpm-ostree install \
+    /tmp/rpms/*.rpm \
+    fedora-repos-archive
+
+rpm-ostree install /tmp/akmods-rpms/ublue-os/ublue-os-akmods-addons*.rpm
+for REPO in $(rpm -ql ublue-os-akmods-addons|grep ^"/etc"|grep repo$); do
+    echo "akmods: enable default entry: ${REPO}"
+    sed -i '0,/enabled=0/{s/enabled=0/enabled=1/}' ${REPO}
+done
+
+rpm-ostree install /tmp/akmods-rpms/kmods/*.rpm
+
+for REPO in $(rpm -ql ublue-os-akmods-addons|grep ^"/etc"|grep repo$); do
+    echo "akmods: disable per defaults: ${REPO}"
+    sed -i 's@enabled=1@enabled=0@g' ${REPO}
+done
+
+if [[ "${#INCLUDED_PACKAGES[@]}" -gt 0 && "${#EXCLUDED_PACKAGES[@]}" -eq 0 ]]; then
+    rpm-ostree install \
+        ${INCLUDED_PACKAGES[@]}
+
+elif [[ "${#INCLUDED_PACKAGES[@]}" -eq 0 && "${#EXCLUDED_PACKAGES[@]}" -gt 0 ]]; then
+    rpm-ostree override remove \
+        ${EXCLUDED_PACKAGES[@]}
+
+elif [[ "${#INCLUDED_PACKAGES[@]}" -gt 0 && "${#EXCLUDED_PACKAGES[@]}" -gt 0 ]]; then
+    rpm-ostree override remove \
+        ${EXCLUDED_PACKAGES[@]} \
+        $(printf -- "--install=%s " ${INCLUDED_PACKAGES[@]})
+
+else
+    echo "No packages to install."
+
+fi
+
+systemctl unmask dconf-update.service && \
+systemctl enable dconf-update.service && \
+systemctl enable tailscaled.service && \
+sed -i 's/#DefaultTimeoutStopSec.*/DefaultTimeoutStopSec=15s/' /etc/systemd/user.conf && \
+sed -i 's/#DefaultTimeoutStopSec.*/DefaultTimeoutStopSec=15s/' /etc/systemd/system.conf && \
+
+rm -f /etc/yum.repos.d/tailscale.repo
